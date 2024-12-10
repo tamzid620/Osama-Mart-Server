f0458901f813ab866b1118b3f077f6d62d07a75e
tamzid620 <tamzid620@gmail.com>
2024-12-10 17:21:51 +0600

    gallery json aded with collection

--- index.js
+++ index.js
@@ -1,43 +1,60 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 
+dotenv.config();
 const app = express();
 const port = 7000;
 
+const corsOptions = {
+  origin: 'http://localhost:3000', // Frontend URL
+  optionsSuccessStatus: 200,
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true, // Allow cookies or authorization headers if needed
+};
+app.use(cors(corsOptions));
 
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 
 
-// const uri = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.qtemx5j.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
-const uri = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.qtemx5j.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0 ` 
+const uri = `mongodb+srv://${dbUsername}:${dbPassword}@cluster0.qtemx5j.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`
 
 const client = new MongoClient(uri);
 
 let toysCollection;
-
+////////////////////////////////////////////////
 client.connect()
   .then(() => {
-    const db = client.db(osamaMart);
-    toysCollection = db.collection('alltoys');
-    console.log('Connected to MongoDB');
+    const db = client.db(dbName);
+    allToysCollection = db.collection('alltoys');
+    specialGalleryCollection = db.collection('specialGallary');
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
-
-
+// -------------------------------------------
 app.get('/all-toys', (req, res) => {
-  toysCollection.find().toArray()
+  allToysCollection.find().toArray()
     .then((data) => res.json(data))
     .catch((error) => {
       console.error('Error fetching data:', error);
-      res.status(500).json({ error: 'Failed to fetch chef data' });
+      res.status(500).json({ message: 'Error fetching data' });
+    });
+});
+app.get('/special-gallery', (req, res) => {
+  specialGalleryCollection.find().toArray()
+    .then((data) => res.json(data))
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ message: 'Error fetching data' });
     });
 });
 
+//////////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
   res.send('Express js connect successfully!');
 });
