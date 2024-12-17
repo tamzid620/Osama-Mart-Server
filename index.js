2b64f48effb2fef165db2bb79c00b60295f73a2c
tamzid620 <tamzid620@gmail.com>
2024-12-17 15:03:29 +0600

    all toy dynamic id details id created

--- index.js
+++ index.js
@@ -54,6 +54,23 @@ app.get('/special-gallery', (req, res) => {
     });
 });
 
+app.get('/all-toys/:id', (req, res) => {
+  const toyId = req.params.id;
+  allToysCollection.findOne({ id: parseInt(toyId) }) 
+      .then((toy) => {
+      if (toy) {
+        res.json(toy);
+      } else {
+        res.status(404).json({ message: 'Toy not found' });
+      }
+    })
+    .catch((error) => {
+      console.error('Error fetching toy:', error);
+      res.status(500).json({ message: 'Error fetching toy' });
+    });
+});
+
+
 //////////////////////////////////////////////////////////////////
 app.get('/', (req, res) => {
   res.send('Express js connect successfully!');
